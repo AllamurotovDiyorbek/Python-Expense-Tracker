3f80613fbb3042d052cca16077688b05a2238859
AllamurotovDiyorbek <135515651+AllamurotovDiyorbek@users.noreply.github.com>
2025-07-27 21:57:04 +0500

    e

--- utils.py
+++ utils.py
@@ -1,63 +1,16 @@
 import os
 import csv
-
 FILENAME = "expenses.csv"
 
 def add_expense(date, category, amount):
-    if not os.path.exists(FILENAME):
-        open(FILENAME, 'x').close()
-
-    fieldnames = ['date', 'category', 'amount']
-    with open(FILENAME) as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames)
-
-        next(reader)
-
-        rows = list(reader)
-    
-    rows.append({
-        'date': date,
-        'category': category,
-        'amount': amount
-    })
-
-    with open(FILENAME, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames)
-        writer.writeheader()
-        writer.writerows(rows)
-
+    pass
 def read_all_expenses():
-    if not os.path.exists(FILENAME):
-        open(FILENAME, 'x').close()
-
-    rows = []
-    fieldnames = ['date', 'category', 'amount']
-    with open(FILENAME) as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames)
-
-        next(reader)
-
-        rows = list(reader)
-
-    return rows
-
+    pass
 def calculate_total(expenses):
-    total = sum(map(lambda expense: float(expense['amount']), expenses))
-    return total
-
+    pass
 def filter_by_date(expenses, search_date):
-    filtered_expenses = list(filter(
-        lambda expense: expense['date'] == search_date,
-        expenses
-    ))
-    return filtered_expenses
-
+    pass
 def filter_by_category(expenses, search_category):
-    filtered_expenses = list(filter(
-        lambda expense: expense['category'] == search_category,
-        expenses
-    ))
-    return filtered_expenses
-
+    pass
 def format_expense(row):
     pass
